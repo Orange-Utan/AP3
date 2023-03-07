11c75409ef34dfa62f52557e62fd2b9b02db1bf5
AberDalliJa <dallidallija@gmail.com>
2023-03-07 12:58:08 +0100

    .

---
+++ getDatabetter.py
@@ -0,0 +1,46 @@
+import csv
+import numpy as np
+
+cr = csv.reader(open('TestGetDAta.csv', "r"))
+outputArray = []
+oA = []
+
+arr = list(range(100))  # adjust to needed
+x = 0
+for row in cr:
+    arr[x] = row
+    x += 1
+
+
+
+startAtLine = 3
+spalte = 0
+spalte_end = 1
+endAtLine = 10
+
+
+s = spalte
+while s <= spalte_end:
+
+    y = int(startAtLine)
+    while y <= endAtLine:
+        if arr[y][s] != '':
+            oA.append(arr[y][s])
+        y += 1
+
+    a = 0
+    while a < np.size(oA):
+        amount = oA[a]
+        maketrans = amount.maketrans
+        amount = amount.translate(maketrans(",", "."))
+        outputArray.append(amount)
+        outputArray[a] = float(outputArray[a])
+        a+=1
+
+
+    print(outputArray)
+    outputArray = []
+    amount = []
+    maketrans = []
+    oA = []
+    s += 1

282d32c7ba8bf9e30beae5b2c0dff6c51b595fbe
Orange-Utan <frowintmp@gmail.com>
2023-03-08 15:34:42 +0100

    7

---
+++ OPA/OPA_Aufgabe7.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import math
+import scipy
+from scipy import optimize
+from scipy.optimize import curve_fit
+from uncertainties import unumpy
+from uncertainties.unumpy import uarray, std_devs, nominal_values
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+
+f1strich = (ufloat(7.49,0.019)/100+ufloat(7.26,0.033)/100)/2
+print(f1strich)
+fges = ufloat(12.6,0.25)/100
+t = 3/100
+h = ufloat(3.3, 0.6)/100
+
+f2strich = (t-f1strich)/(-f1strich/fges+1)
+print(f2strich*100)
+

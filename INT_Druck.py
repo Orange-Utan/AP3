cac8d23f059b052af5bdc7520a7a501d4f5bbe7a
AberDalliJa <dallidallija@gmail.com>
2023-03-19 21:31:18 +0100

    .

--- INT_Druck.py
+++ INT_Druck.py
@@ -15,9 +15,10 @@ from matplotlib.ticker import MultipleLocator, AutoMinorLocator
 import  generalFunctions as gf
 
 
-
-mess1 = uarray([0.0, 0.05, 0.1, 0.18, 0.24, 0.3, 0.35, 0.4, 0.48, 0.52, 0.59, 0.64, 0.71, 0.76, 0.82],0.01/2*np.sqrt(6))
-mess2 = uarray([0.0, 0.06, 0.1, 0.16, 0.22, 0.28, 0.34, 0.38, 0.44, 0.5, 0.56, 0.63, 0.69, 0.74, 0.8],0.01/2*np.sqrt(6))
+length = ufloat(4.96,0.005)/100
+lamda = lamda = ufloat(532,1)/(10**(9))
+mess1 =-uarray([0.0, 0.05, 0.1, 0.18, 0.24, 0.3, 0.35, 0.4, 0.48, 0.52, 0.59, 0.64, 0.71, 0.76, 0.82],0.01/2*np.sqrt(6))
+mess2 =-uarray([0.0, 0.06, 0.1, 0.16, 0.22, 0.28, 0.34, 0.38, 0.44, 0.5, 0.56, 0.63, 0.69, 0.74, 0.8],0.01/2*np.sqrt(6))
 max = uarray([0.0, 3.0, 6.0, 9.0, 12.0, 15.0, 18.0, 21.0, 24.0, 27.0, 30.0, 33.0, 36.0, 39.0, 42.0],1/2*np.sqrt(6))
 
 mittelnorm = []
@@ -51,7 +52,9 @@ def tabularX():
     texheader = texheader + ' &'
     # texdata = "\\hline\n"
     texdata = ""
-    for i in range(0,len(sorted(spalte)[0])-1):
+
+
+    for i in range(0,len(spalte.values())-1):
         texdata += '\\hline'
         for label in sorted(spalte):
             texdata += str(gf.ufloatToTexStr(spalte[label][i])) + ' & '
@@ -69,4 +72,62 @@ def tabularX():
     print("\\caption{Berücksichtigte Ungenauigkeiten}")
     print("\\label{tab:Ungenauigkeiten}")
     print("\\end{table}")
-tabularX()
+
+def brech(N):
+    return -((N+1/2)*lamda/(2*length))
+
+fig, ax = plt.subplots()
+ax.errorbar(
+        nominal_values(mess1),
+        nominal_values(brech(max)),
+        label = r'Messreihe 1',
+        color = 'blue',
+        linestyle='',
+        marker='.',
+        capsize=1.5,
+        #elinewidth=1.2,
+        xerr=std_devs(mess1),
+        yerr=std_devs(brech(max))
+        )
+ax.errorbar(
+        nominal_values(mess2),
+        nominal_values(brech(max)),
+        label = r'Messreihe 2',
+        color = 'green',
+        linestyle='',
+        marker='.',
+        capsize=1.5,
+        #elinewidth=1.2,
+        xerr=std_devs(mess2),
+        yerr=std_devs(brech(max))
+        )
+ax.errorbar(
+        nominal_values(mittel),
+        nominal_values(brech(max)),
+        label = r'Mittelwert',
+        color = 'red',
+        linestyle='',
+        marker='.',
+        capsize=1.5,
+        #elinewidth=1.2,
+        xerr=std_devs(mittel),
+        yerr=std_devs(brech(max))
+        )
+
+Temp=295.7
+def func(p,a,b):
+    return a*p+b
+
+
+popt3, pcov3 = curve_fit(func, xdata=nominal_values(mittel), ydata=nominal_values(brech(max)))
+ax.plot(np.linspace(-1, 0, 100), func(np.linspace(-1, 0, 100), popt3[0],popt3[1]),
+        'red',
+        label='Fit an den Mittelwert: $\Delta \, n = 2,76\cdot 10^{-4} \cdot \Delta\, p + 4,36 \cdot 10^{-6}$')
+
+print(popt3)
+print(np.sqrt(pcov3))
+ax.set_xlabel("$\Delta$p in bar")
+ax.set_ylabel(r"$\Delta$n")
+plt.legend()
+plt.show()
+
